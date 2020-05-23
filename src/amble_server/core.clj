57071c6836f19de22e9869d83d5413144a89214c
Ben Davis <ben.davis@signalpath.com>
2020-05-23 12:59:08 -0400

    Opts for 'lein new compojure amble-server' instead.

--- src/amble_server/core.clj
+++
@@ -1,9 +0,0 @@
-(ns amble-server.core
-  (:require [compojure.core :refer :all]
-            [compojure.route :as route]))
-
-(defroutes app
-           (POST "/game" [] "<h1>Hello World</h1>"))
-
-(defn -main [& args]
-  (println "helloooooo!!!!"))
--- test/amble_server/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns amble-server.core-test
-  (:require [clojure.test :refer :all]
-            [amble-server.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
