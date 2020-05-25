f2fc4684a126106187919805476d149b6b632adf
Ben Davis <ben.davis@signalpath.com>
2020-05-25 14:45:52 -0400

    no idea

--- src/amble_server/handler.clj
+++ src/amble_server/handler.clj
@@ -2,12 +2,18 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
-            [amble-server.services.game :as game]))
+            [amble-server.services.game :as game]
+            [ring.middleware.cors :as mcors]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET "/game/:game-id" [] game/get-by-id)
   (POST "/game" [] game/create)
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes api-defaults))
+  (mcors/wrap-cors (wrap-defaults app-routes api-defaults)
+                   :access-control-allow-origin ["http://localhost:3001"]
+                   :access-control-allow-headers ["Content-Type"]))
+
+
--- src/amble_server/services/game.clj
+++ src/amble_server/services/game.clj
@@ -3,7 +3,21 @@
     [ring.util.response :as response-util]
     [compojure.response :as response]))
 
+(defn get-by-id [req]
+  (println "Sending get game by id response.")
+  (let [game-id "fortyTwo"
+        response-raw
+        (response-util/status (response/render game-id req)
+                              200)]
+
+    (println response-raw)
+    (assoc-in response-raw
+              [:headers
+               "Access-Control-Allow-Origin"]
+              "http://localhost")))
+      
+
 (defn create [req]
-  (let [gameId "immaPretendGameId"]
-    (response-util/status (response/render gameId req)
+  (let [game-id "immaPretendGameId"]
+    (response-util/status (response/render game-id req)
                           201)))
