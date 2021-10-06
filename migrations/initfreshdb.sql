2f657b781a418daa5507ab0e43af32854270c63f
ben <bendavisnc@gmail.com>
2021-10-05 21:23:38 -0400

    Fixes delete cascade for move.

--- migrations/initfreshdb.sql
+++ migrations/initfreshdb.sql
@@ -3,19 +3,19 @@ CREATE TABLE IF NOT EXISTS "Game" (
 	"id"	TEXT,
 	PRIMARY KEY("id")
 );
-CREATE TABLE IF NOT EXISTS "Move" (
-	"id"	TEXT,
-	"player_id"	TEXT,
-	"game_id"	TEXT,
-	"move"	TEXT,
-	FOREIGN KEY("game_id") REFERENCES "Game"("id") ON DELETE CASCADE,
-	FOREIGN KEY("player_id") REFERENCES "Player"("id") ON DELETE CASCADE,
-	PRIMARY KEY("id")
-);
 CREATE TABLE IF NOT EXISTS "Player" (
 	"id"	TEXT,
 	"game_id"	TEXT,
 	FOREIGN KEY("game_id") REFERENCES "Game"("id") ON DELETE CASCADE,
 	UNIQUE("id", "game_id")
+);
+CREATE TABLE IF NOT EXISTS "Move" (
+	"id"	TEXT,
+	"player_id"	TEXT,
+	"game_id"	TEXT,
+	"move"	TEXT,
+--	FOREIGN KEY("game_id") REFERENCES "Game"("id") ON DELETE CASCADE,
+	PRIMARY KEY("id"),
+	FOREIGN KEY("game_id", "player_id") REFERENCES "Player"("game_id", "id") ON DELETE CASCADE
 );
 COMMIT;
