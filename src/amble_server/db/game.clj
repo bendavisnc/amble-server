e2031dd18fab36a81f500b3ea920ba97cd4d38f8
ben <bendavisnc@gmail.com>
2021-10-09 18:09:25 -0400

    Adds db layer code refactor cleanup.

--- src/amble_server/db/game.clj
+++ src/amble_server/db/game.clj
@@ -1,39 +1,34 @@
 (ns amble-server.db.game
-  (:require [amble-server.db.game-sql :as game-sql]))
+  (:require [amble-server.db.game-sql :as game-sql])
+  (:refer-clojure :exclude [find]))
 
 (def success ::sucess)
 (def failure ::failure)
 
-(defn create!
-  [id]
-  (try (let [db-result
-             (game-sql/create! id)]
-         (if (not (pos? db-result))
-           {failure (new IllegalStateException (format "Bad db result \"%s\".",
-                                                       db-result))}
-           {success id}))
+(defn create! [id]
+  (try (let [row-add-count  (game-sql/create! id)]
+         (when (not (= 1 row-add-count))
+           (throw (new IllegalStateException (format "Bad db result \"%s\"."
+                                                     row-add-count))))
+         {success id})
        (catch Throwable e
          {failure e})))
 
 (defn find [id]
-  (try (let [found*
-             (game-sql/find id)
-             found
-             (first (map :id found*))]
-         {success found})
+  (try (let [game-id-found (first (for [game-row (game-sql/find id)]
+                                    (:id game-row)))]
+         {success game-id-found})
        (catch Throwable e
          {failure e})))
 
 (defn delete!
   "Deletes the game with the given game id."
   [id]
-  (try (let [db-result
-             (game-sql/delete! id)]
-         (if (not (pos? db-result))
-           {failure (new IllegalStateException (format "Bad db result \"%s\".",
-                                                       db-result))}
-           {success id}))
+  (try (let [row-delete-count  (game-sql/delete! id)]
+         (when (not (= 1 row-delete-count))
+           (throw (new IllegalStateException (format "Bad db result \"%s\"."
+                                                     row-delete-count))))
+         {success id})
        (catch Throwable e
          {failure e})))
-
 
--- src/amble_server/db/player.clj
+++ src/amble_server/db/player.clj
@@ -1,34 +1,32 @@
 (ns amble-server.db.player
-  (:require [amble-server.db.player-sql :as player-sql]))
+  (:require [amble-server.db.player-sql :as player-sql])
+  (:refer-clojure :exclude [find]))
 
 (def success ::success)
 (def failure ::failure)
 
-(defn add!
-  [game-id, id]
-  (try (let [db-result
-             (player-sql/add! game-id, id)]
-         (if (not (pos? db-result))
-           {failure (new IllegalStateException (format "Bad db result \"%s\".",
-                                                       db-result))}
-           {success id}))
+(defn add! [game-id, id]
+  (try (let [row-add-count  (player-sql/add! game-id, id)]
+         (when (not (= 1 row-add-count))
+           (throw (new IllegalStateException (format "Bad db result \"%s\"."
+                                                     row-add-count))))
+         {success id})
+       (catch Throwable e
+         {failure e})))
+
+(defn find-all [game-id]
+  (try (let [player-ids (vec (for [player-row (player-sql/find game-id)]
+                               (:id player-row)))]
+         {success player-ids})
        (catch Throwable e
          {failure e})))
 
 (defn find
   ([game-id]
-   (try (let [found*
-              (player-sql/find game-id)
-              found
-              (map :id found*)]
-          {success found})
-        (catch Throwable e
-          {failure e})))
+   (find-all game-id))
   ([game-id, id]
-   (try (let [found*
-              (player-sql/find game-id, id)
-              found
-              (first (map :id found*))]
-          {success found})
+   (try (let [player-id (first (for [player-row (player-sql/find game-id, id)]
+                                 (:id player-row)))]
+          {success player-id})
         (catch Throwable e
           {failure e}))))
