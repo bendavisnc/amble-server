8948f23c61aae718622b5565761255908d44035f
ben <bendavisnc@gmail.com>
2022-08-10 19:32:22 -0400

    Updates move async to use a map.

--- src/amble_server/move_async.clj
+++ src/amble_server/move_async.clj
@@ -9,44 +9,52 @@
 
 (def websockets-path "/move/async")
 
-(def subscribers (atom []))
+(def subscribers (atom {}))
 
 (def latest-move-index-chan (async/chan))
 
+(defn add-subscriber! [subscriber]
+  (.info log "Adding subscriber to subscribers (count, '{}'), '{}'", (count (deref subscribers)) subscriber)
+  (swap! subscribers 
+         (fn [subs]
+           (assoc subs (:subscriber-id subscriber) subscriber)))
+  (.info log "Added subscriber to subscribers (count, '{}'), '{}'", (count (deref subscribers)) subscriber))
+
 (defn remove-subscriber! [subscriber]
   (.info log "Removing subscriber from subscribers (count, '{}'), '{}'", (count (deref subscribers)) subscriber)
-  (swap! subscribers (fn [subs]
-                       (remove #{subscriber} subs)))
-                      ;;  (filter (fn [s]
-                                ;;  (= subscriber s)
-                              ;;  subs]))
+  (swap! subscribers 
+         (fn [subs]
+           (dissoc subs (:subscriber-id subscriber))))
   (.debug log "Removed subscriber from subscribers (count, '{}'), '{}'", (count (deref subscribers)) subscriber))
   ;; (swap! subscribers #(filter (= % subscriber))))
 
 (defn notify-subscribers! [latest-move-index]
-  (doseq [subscriber @subscribers]
+  (doseq [subscriber-key (keys (deref subscribers))]
     (try
-      (subscriber (str latest-move-index))
+      (.info log "Updating " (count (deref subscribers)) " subscribers.")
+      (let [{:keys [update-subscriber!]} ((deref subscribers)
+                                          subscriber-key)]
+        (update-subscriber! (str latest-move-index)))
       (catch Exception e
         (do
           (.error log "Something bad happened while trying to notify subscriber.", e)
-          (remove-subscriber! subscriber))))))
-                    
-;; Reads from the input chan and notifies subscribers.
-(go-loop []
-  (let [latest-move-index (async/<! latest-move-index-chan)]
-    (.info log latest-move-index)
-    (notify-subscribers! latest-move-index)
-    (recur)))
+          (when-let [subscriber-to-remove ((deref subscribers))]
+                                           subscriber-key 
+            (remove-subscriber! subscriber-to-remove)))))))   
+
+(defn subscriber 
+  "Creates a subscriber instance.
+   Its input should be the value that the client cares about as notification input,
+   currently the number index of the latest move made."
+  [ws]
+  {:subscriber-id 
+   (.hashCode ws)
+   :update-subscriber!
+   (partial jetty/send! ws)})
 
 (defn on-connect [ws & args]
   (.info log "New move async connection!")
-  (.debug log args)
-  (.info log (format "Currently there's %d subscribers."
-                     (count (deref subscribers))))  
-  (swap! subscribers conj (partial jetty/send! ws))
-  (.info log (format "Now there's %d subscribers."
-                     (count (deref subscribers))))  
+  (add-subscriber! (subscriber ws))
   nil)
 
 (defn on-error [ws & args]
@@ -54,13 +62,27 @@
   (.debug log args)
   nil)
 
+(defn on-close [ws & args]
+  (.info log "Existing async channel to close.")
+  (when-let [subscriber-to-remove 
+             ((deref subscribers)
+              (.hashCode ws))]
+    (remove-subscriber! subscriber-to-remove))   
+  nil)
+
 
 (def handlers {:on-connect on-connect
                :on-error on-error 
                :on-text nil
-               :on-close nil
+               :on-close on-close
                :on-bytes nil})
+
+;; Reads from the input chan and notifies subscribers.
+(go-loop []
+  (let [latest-move-index (async/<! latest-move-index-chan)]
+    (.info log latest-move-index)
+    (notify-subscribers! latest-move-index)
+    (recur)))
 
 (defn init! [latest-move-index-chan]
   (async/pipe latest-move-index-chan amble-server.move-async/latest-move-index-chan))
-
