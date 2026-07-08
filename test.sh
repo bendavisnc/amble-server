aad02fe2c4dee1d5b97a919a29b5ecd86ec6cf31
bendavisnc <bendavisnc@gmail.com>
2026-07-08 17:58:05 -0400

    Revisits tests after middleware changes.

--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
+./createdb.sh
 clj -X:test
--- test/amble_server/handler_test.clj
+++ test/amble_server/handler_test.clj
@@ -1,22 +1,20 @@
 (ns amble-server.handler-test
-  (:require [amble-server.api.handler :as api-handler]
-            [clojure.data.json :as json]
-            [clojure.test :refer [deftest is testing]]
-            [ring.mock.request :as mock-request]))
+  (:require
+    [amble-server.main :as main]
+    [clojure.data.json :as json]
+    [clojure.test :refer [deftest is testing]]
+    [ring.mock.request :as mock-request]))
 
 (def test-game-id "TheTestGame")
 
 (deftest test-amble-server-api
   (testing "create game"
-    (let [
-          _ (api-handler/handler
+    (let [_ (main/handler
              (mock-request/request :delete (str "/game/" test-game-id)))
-
-          create-game-request  (mock-request/request :post
-                                                     "/game"
-                                                     (json/write-str
-                                                      {:game-id test-game-id}))
-          create-game-response (api-handler/handler create-game-request)]
+          create-game-request  (-> (mock-request/request :post "/game")
+                                   (mock-request/json-body {:game-id
+                                                            test-game-id}))
+          create-game-response (main/handler create-game-request)]
       (is (= 201
              (:status create-game-response)))
       (is (= {:game-id test-game-id}
@@ -25,7 +23,7 @@
   (testing "get game"
     (let [get-game-request  (mock-request/request :get
                                                   (str "/game/" test-game-id))
-          get-game-response (api-handler/handler get-game-request)]
+          get-game-response (main/handler get-game-request)]
       (is (= 200
              (:status get-game-response)))
       (is (= {:game-id test-game-id}
