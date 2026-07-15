68c808ad5ebfcc685bd00129c57460bf2667e1fa
bendavisnc <bendavisnc@gmail.com>
2026-07-15 12:54:11 -0400

    Adds logging to shutdown handler.

--- src_dbbackup/amble_dbbackup/shutdownwrite/main.clj
+++ src_dbbackup/amble_dbbackup/shutdownwrite/main.clj
@@ -11,15 +11,19 @@
     (git/git-commit repo "Backup update")
     (git/with-credentials {:login config/dbbackup-username
                            :pw    config/dbbackup-private-key}
-                          (git/git-push repo "origin" "main"))
-    (println "Backup pushed successfully.")))
+                          (git/git-push repo "origin" "main"))))
 
 (defn write-at-shutdown
   []
   (.addShutdownHook (Runtime/getRuntime)
                     (new Thread
                          (fn []
-                           (write))))
+                           (try 
+                            (println "Backup push initiated.")
+                            (write)
+                            (println "Backup pushed successfully.")
+                            (catch Throwable e
+                              (throw (ex-info "Something bad happened while trying to push backup during shutdown." {} e)))))))
 
   (println "Shutdown handler started, waiting for SIGTERM...")
   (let [lock (new Object)]
@@ -31,8 +35,7 @@
   (if-not config/dbbackup-remote
     (println "`dbbackup-remote` not set in config, skipping backup write.")
     ;; else
-    (do (write-at-shutdown)
-        (println "Successfully handled shutdown."))))
+    (write-at-shutdown)))
 
 
 
