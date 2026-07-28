f2201ec1b3047751fe67f82f0d158043f7e2d922
bendavisnc <bendavisnc@gmail.com>
2026-07-28 18:30:04 -0400

    Leverages `amble-board` in player api.

--- src/amble_server/api/player.clj
+++ src/amble_server/api/player.clj
@@ -4,11 +4,26 @@
     [amble-server.resource.game :as game-resource]
     [amble-server.resource.move :as move-resource]
     [amble-server.resource.player :as player-resource]
-    [clojure.edn :as edn]
+    [clojure.data.json :as json]
     [clojure.java.io :as io]
     [ring.util.response :as response-util]
     [taoensso.timbre :as log]))
 
+(defmacro data-from-json
+  [filename]
+  (let [resource (io/resource filename)
+        _ (assert resource (format "Could not find `%s` resource." filename))
+        v        (json/read-str (slurp resource) :key-fn keyword)]
+    v))
+
+(def board (data-from-json "board.json"))
+
+(def player-initial-placements
+  (let [player-indexes (data-from-json "board-players.json")]
+    (into {}
+          (for [[player-id indexes] player-indexes
+                :let [positions (vec (map board indexes))]]
+            [player-id positions]))))
 
 (defn get-all
   [req]
@@ -17,51 +32,16 @@
                         :game-id
                         keyword)]
     (try (let [game-found (game-resource/get game-id)]
-           (cond (not game-found) (response-util/status req 404)
-                 :else            (response-util/response
-                                   (player-resource/get-all game-id))))
+           (if (not game-found)
+             (response-util/status req 404)
+             (response-util/response
+              (player-resource/get-all game-id))))
          (catch Throwable e
            (throw (ex-info
                    "An error occurred during game player get all."
                    {:game-id game-id}
                    e))))))
 
-
-
-(defn crude-player-indexes-map
-  [player-id]
-  (let [ordered
-        [:player-one :player-two :player-three :player-four :player-five
-         :player-six]
-        id-index (.indexOf ordered player-id)
-        _ (assert (not (neg-int? id-index)))
-        index-list
-        [[111 112 113 114 115 116 117 118 119 120]
-         [65 74 76 84 86 88 95 97 99 101]
-         [10 11 12 13 20 22 24 33 35 45]
-         [0 1 2 3 4 5 6 7 8 9]
-         [19 21 23 25 32 34 36 44 46 55]
-         [75 85 87 96 98 100 107 108 109 110]]
-        player-coord-indexes (index-list id-index)
-        board-coords-resource (io/resource "board.json")
-        _ (when-not board-coords-resource
-            (throw (new Exception "Could not find board.json resource.")))
-        boord-coords
-        (edn/read-string (slurp board-coords-resource))
-        player-coords (vec (map (fn [i]
-                                  (nth boord-coords i))
-                                player-coord-indexes))]
-    player-coords))
-
-(defn merge-positions-and-moves
-  [simple-position-list moves-existing]
-  (reduce (fn [acc move]
-            (assoc acc
-                   (:player-piece-index move) ;; maybe revisit
-                   [(:x move)
-                    (:y move)]))
-          simple-position-list
-          moves-existing))
 
 (defn get
   "Returns either an existing player's current position as a list of two value vectors, or a not found response, or an error response."
@@ -74,40 +54,35 @@
                           :params
                           :player-id
                           keyword)]
-    (try (let [
+    (try
+      (let [player-existing (player-resource/get game-id player-id)]
+        (if
+          (nil? player-existing)
+          (do
+            (log/info ::get
+                      "No player found for game."
+                      {:game-id   game-id
+                       :player-id player-id})
+            (-> (response-util/response [])
+                (response-util/status 404)))
+          ;; else
+          (let [moves-existing (move-resource/get-by-player-id game-id
+                                                               player-id)
+                positions      (reduce
+                                (fn [acc {:keys [x y :player-piece-index]}]
+                                  (assoc acc player-piece-index [x y]))
+                                (or (player-initial-placements player-id)
+                                    (throw (ex-info "Bad `player-id`."
+                                                    {:player-id player-id})))
+                                moves-existing)]
+            (-> (response-util/response positions)
+                (response-util/status 200)))))
 
-               player-existing (player-resource/get game-id player-id)
-               player-simple-position-list (crude-player-indexes-map player-id)]
-           (cond
-             (nil? player-existing)
-             (do
-               (log/info ::get
-                         "No player found for game."
-                         {:game-id   game-id
-                          :player-id player-id})
-               (-> (response-util/response [])
-                   (response-util/status 404)))
-             (empty? player-simple-position-list)
-             (do (log/info ::get
-                           "Returning existing player with no moves existing."
-                           {:game-id   game-id
-                            :player-id player-id})
-                 (-> (response-util/response (crude-player-indexes-map
-                                              player-id))
-                     (response-util/status 200)))
-             :else
-             (let [moves-existing
-                   (move-resource/get-by-player-id game-id
-                                                   player-id)]
-               (-> (response-util/response (merge-positions-and-moves
-                                            player-simple-position-list
-                                            moves-existing))
-                   (response-util/status 200)))))
-
-         (catch Throwable e
-           (throw
-            (ex-info
-             "Something bad happened when trying to get a player from the game."
-             {:game-id   game-id
-              :player-id player-id}
-             e))))))
+      (catch Throwable e
+        (throw
+         (ex-info
+          "Something bad happened when trying to get a player from the
+             game."
+          {:game-id   game-id
+           :player-id player-id}
+          e))))))
--- test/amble_server/handler_test.clj
+++ test/amble_server/handler_test.clj
@@ -37,46 +37,42 @@
           get-players-response (main/handler get-players-request)]
       (is (= 200
              (:status get-players-response)))
-      (is (= ["player-one" "player-two" "player-three" "player-four" "player-five" "player-six"]
-             (json/read-str (:body get-players-response)))))) 
+      (is (= ["player-one" "player-two" "player-three" "player-four"
+              "player-five" "player-six"]
+             (json/read-str (:body get-players-response))))))
 
   (testing "get player"
     (let [get-player-request  (mock-request/request
-                                :get
-                                (str "/game/" test-game-id "/player/player-one"))
+                               :get
+                               (str "/game/" test-game-id "/player/player-one"))
           get-player-response (main/handler get-player-request)]
       (is (= 200
              (:status get-player-response)))
-      (is (= [[0.425 0.7165] [0.475 0.7165] [0.525 0.7165] [0.575 0.7165] [0.45 0.7598] [0.5 0.7598] [0.55 0.7598] [0.475 0.8031] [0.525 0.8031] [0.5 0.8464]]
-             (json/read-str (:body get-player-response)))))) 
+      (is (= [[0.5 0.8464] [0.525 0.8031] [0.475 0.8031] [0.55 0.7598]
+              [0.5 0.7598] [0.45 0.7598] [0.575 0.7165] [0.525 0.7165]
+              [0.475 0.7165] [0.425 0.7165]]
+             (json/read-str (:body get-player-response))))))
 
   (testing "get player (after made move)"
     (let [_ (main/handler
-             (-> (mock-request/request :post (str "/game/" test-game-id "/player/player-one/move/0"))
+             (-> (mock-request/request
+                  :post
+                  (str "/game/" test-game-id "/player/player-one/move/0"))
                  (mock-request/json-body {:game-id test-game-id
-                                          :x 0.5
-                                          :y 0.5})))
+                                          :x       0.5
+                                          :y       0.5})))
 
           get-player-request  (mock-request/request
-                                :get
-                                (str "/game/" test-game-id "/player/player-one"))
+                               :get
+                               (str "/game/"
+                                    test-game-id
+                                    "/player/player-one"))
           get-player-response (main/handler get-player-request)]
       (is (= 200
              (:status get-player-response)))
-      (is (= [[0.5 0.5] [0.475 0.7165] [0.525 0.7165] [0.575 0.7165] [0.45 0.7598] [0.5 0.7598] [0.55 0.7598] [0.475 0.8031] [0.525 0.8031] [0.5 0.8464]]
-             (json/read-str (:body get-player-response))))))) 
-
-
-
-
-
-
-
-
-
-
-
-
-
+      (is (= [[0.5 0.5] [0.525 0.8031] [0.475 0.8031] [0.55 0.7598] [0.5 0.7598]
+              [0.45 0.7598] [0.575 0.7165] [0.525 0.7165] [0.475 0.7165]
+              [0.425 0.7165]]
+             (json/read-str (:body get-player-response)))))))
 
 
